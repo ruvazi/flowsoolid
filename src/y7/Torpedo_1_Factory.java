4831611dd9f0c07938f7a993a51110d90c9e65a9
ruvazi <Runevazi@gmail.com>
2014-12-15 10:23:39 +0100

    TrololololoLLOOO

---
+++ src/y7/Torpedo_1_Factory.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package y7;
+
+import battleship.examples.*;
+import battleship.interfaces.BattleshipsPlayer;
+import tournament.player.PlayerFactory;
+
+/**
+ *
+ * @author Tobias Grundtvig
+ */
+public class Torpedo_1_Factory implements PlayerFactory<BattleshipsPlayer>
+{
+    private static int nextID = 1;
+    private final int id;
+
+    public Torpedo_1_Factory()
+    {
+        id = nextID++;
+    }
+    
+    
+    @Override
+    public BattleshipsPlayer getNewInstance()
+    {
+        return new Torpedo_1();                  // vores player navn her
+    }
+
+    @Override
+    public String getID()
+    {
+        return "RND:"+id;
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Random player " + id;               //return vores player navn
+    }
+    
+}

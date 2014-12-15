f770d7519b1a4c7329881fcfe44b41970b5ac069
Jonas Rafn <hjernemanden@gmail.com>
2014-12-15 10:53:17 +0100

    Tilføjet alt muligt

--- src/y7/Torpedo_1.java
+++ src/y7/Torpedo_1.java
@@ -10,41 +10,38 @@ import battleship.interfaces.Fleet;
 import battleship.interfaces.Position;
 import battleship.interfaces.Board;
 import battleship.interfaces.Ship;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  *
  * @author Tobias
  */
-public class Torpedo_1 implements BattleshipsPlayer
-{
+public class Torpedo_1 implements BattleshipsPlayer {
+
     private final static Random rnd = new Random();
     private int sizeX;
     private int sizeY;
 
-    public Torpedo_1()
-    {
+    private ArrayList<Position> posArray = new ArrayList<Position>();
+
+    public Torpedo_1() {
     }
 
     @Override           // leg her
-    public void placeShips(Fleet fleet, Board board)
-    {
+    public void placeShips(Fleet fleet, Board board) {
         sizeX = board.sizeX();
         sizeY = board.sizeY();
-        for(int i = 0; i < fleet.getNumberOfShips(); ++i)
-        {
+        for (int i = 0; i < fleet.getNumberOfShips(); ++i) {
             Ship s = fleet.getShip(i);
             boolean vertical = rnd.nextBoolean();
             Position pos;
-            if(vertical)
-            {
+            if (vertical) {
                 int x = rnd.nextInt(sizeX);
-                int y = rnd.nextInt(sizeY-(s.size()-1));
+                int y = rnd.nextInt(sizeY - (s.size() - 1));
                 pos = new Position(x, y);
-            }
-            else
-            {
-                int x = rnd.nextInt(sizeX-(s.size()-1));
+            } else {
+                int x = rnd.nextInt(sizeX - (s.size() - 1));
                 int y = rnd.nextInt(sizeY);
                 pos = new Position(x, y);
             }
@@ -53,47 +50,60 @@ public class Torpedo_1 implements BattleshipsPlayer
     }
 
     @Override
-    public void incoming(Position pos)
-    {
-        
+    public void incoming(Position pos) {
+
         //Do nothing
     }
 
     @Override
-    public Position getFireCoordinates(Fleet enemyShips)
-    {
+    public Position getFireCoordinates(Fleet enemyShips) {
+
+        Position pos = getFiringPosition();
+        posArray.add(pos);
+
+        for (Position posObject : posArray) {
+            if (posObject == pos) {
+                pos = getFiringPosition();
+            } else {
+
+            }
+        }
+
+        return pos;
+
+    }
+
+    @Override
+    public void hitFeedBack(boolean hit, Fleet enemyShips) {
+        //Do nothing
+    }
+
+    @Override
+    public void startMatch(int rounds) {
+        //Do nothing
+    }
+
+    @Override
+    public void startRound(int round) {
+        //Do nothing
+    }
+
+    @Override
+    public void endRound(int round, int points, int enemyPoints) {
+        //Do nothing
+    }
+
+    @Override
+    public void endMatch(int won, int lost, int draw) {
+        //Do nothing
+    }
+
+    private Position getFiringPosition() {
         int x = rnd.nextInt(sizeX);
         int y = rnd.nextInt(sizeY);
-        return new Position(x,y);
-    }
 
-    @Override
-    public void hitFeedBack(boolean hit, Fleet enemyShips)
-    {
-        //Do nothing
-    }    
+        Position pos = new Position(x, y);
 
-    @Override
-    public void startMatch(int rounds)
-    {
-        //Do nothing
-    }
-
-    @Override
-    public void startRound(int round)
-    {
-        //Do nothing
-    }
-
-    @Override
-    public void endRound(int round, int points, int enemyPoints)
-    {
-        //Do nothing
-    }
-
-    @Override
-    public void endMatch(int won, int lost, int draw)
-    {
-        //Do nothing
+        return pos;
     }
 }

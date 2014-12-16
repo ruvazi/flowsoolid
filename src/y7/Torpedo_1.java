db8df40979e5acf3192ad04936d9427dbd0e7921
Jonas Rafn <hjernemanden@gmail.com>
2014-12-16 12:58:27 +0100

    Tilføjet ny skud måde Tilføjet nye måde at placere skibe

--- src/y7/Torpedo_1.java
+++ src/y7/Torpedo_1.java
@@ -28,9 +28,9 @@ public class Torpedo_1 implements BattleshipsPlayer {
     private ArrayList<Position> posArray = new ArrayList<Position>();
     private ArrayList<Position> posArrayHit = new ArrayList<Position>(); //bliver slettet løbende
     private ArrayList<Position> posArrayHitSave = new ArrayList<Position>(); //gemmer alle positive for hele runden
-    
-    int posCount= 0;
-    
+
+    int posCount = 0;
+
     public Torpedo_1() {
     }
 
@@ -57,93 +57,6 @@ public class Torpedo_1 implements BattleshipsPlayer {
 //
 //            board.placeShip(pos, s, vertical);
 //        }
-        switch (state) {
-            case 0:
-                Position pos1 = new Position(0, 4);
-                Ship s1 = fleet.getShip(2);
-                board.placeShip(pos1, s1, false);
-
-                Position pos2 = new Position(0, 3);
-                Ship s2 = fleet.getShip(3);
-                board.placeShip(pos2, s2, false);
-
-                Position pos3 = new Position(2, 9);
-                Ship s3 = fleet.getShip(3);
-                board.placeShip(pos3, s3, false);
-
-                Position pos4 = new Position(0, 1);
-                Ship s4 = fleet.getShip(4);
-                board.placeShip(pos4, s4, false);
-
-                Position pos5 = new Position(3, 0);
-                Ship s5 = fleet.getShip(5);
-                board.placeShip(pos5, s5, false);
-                break;
-            case 1:
-                Position pos6 = new Position(1, 3);
-                Ship s6 = fleet.getShip(2);
-                board.placeShip(pos6, s6, true);
-
-                Position pos7 = new Position(0, 0);
-                Ship s7 = fleet.getShip(3);
-                board.placeShip(pos7, s7, false);
-
-                Position pos8 = new Position(2, 0);
-                Ship s8 = fleet.getShip(3);
-                board.placeShip(pos8, s8, false);
-
-                Position pos9 = new Position(6, 6);
-                Ship s9 = fleet.getShip(4);
-                board.placeShip(pos9, s9, true);
-
-                Position pos10 = new Position(9, 0);
-                Ship s10 = fleet.getShip(5);
-                board.placeShip(pos10, s10, false);
-                break;
-            case 2:
-                Position pos11 = new Position(1, 3);
-                Ship s11 = fleet.getShip(2);
-                board.placeShip(pos11, s11, false);
-
-                Position pos12 = new Position(7, 3);
-                Ship s12 = fleet.getShip(3);
-                board.placeShip(pos12, s12, true);
-
-                Position pos13 = new Position(6, 9);
-                Ship s13 = fleet.getShip(3);
-                board.placeShip(pos13, s13, false);
-
-                Position pos14 = new Position(4, 4);
-                Ship s14 = fleet.getShip(4);
-                board.placeShip(pos14, s14, false);
-
-                Position pos15 = new Position(3, 5);
-                Ship s15 = fleet.getShip(5);
-                board.placeShip(pos15, s15, true);
-                break;
-            case 3:
-                Position pos16 = new Position(9, 3);
-                Ship s16 = fleet.getShip(2);
-                board.placeShip(pos16, s16, false);
-
-                Position pos17 = new Position(2, 8);
-                Ship s17 = fleet.getShip(3);
-                board.placeShip(pos17, s17, true);
-
-                Position pos18 = new Position(0, 4);
-                Ship s18 = fleet.getShip(3);
-                board.placeShip(pos18, s18, false);
-
-                Position pos19 = new Position(4, 4);
-                Ship s19 = fleet.getShip(4);
-                board.placeShip(pos19, s19, false);
-
-                Position pos20 = new Position(3, 5);
-                Ship s20 = fleet.getShip(5);
-                board.placeShip(pos20, s20, true);
-                break;
-        }
-
     }
 
     @Override
@@ -155,26 +68,16 @@ public class Torpedo_1 implements BattleshipsPlayer {
     @Override
     public Position getFireCoordinates(Fleet enemyShips) {
 
-        Position shot = new Position(nextX, nextY);
-        ++nextX;
-        if (nextX >= sizeX) {
-            nextX = 0;
-            ++nextY;
-            if (nextY >= sizeY) {
-                nextY = 0;
-            }
-        }
-        posArray.add(shot);
-        
-        return shot;
+        Position pos = getFiringPosition();
+        return pos;
     }
 
     @Override
     public void hitFeedBack(boolean hit, Fleet enemyShips) {
-        
-        if(hit){
-        
-        posCount++;
+
+        if (hit) {
+
+            posCount++;
         }
     }
 
@@ -200,18 +103,29 @@ public class Torpedo_1 implements BattleshipsPlayer {
 
     private Position getFiringPosition() {
         Position pos;
-        
+
 //        for (int i = 0; i < 1; i++) {
-        if (posCount>0) {
+        if (posCount > 0) {
             pos = posArray.get(posArray.size());
             posArrayHit.add(pos);
             posArrayHitSave.add(pos);
-            posCount--;
-        }   
-        
-        int x = rnd.nextInt(sizeX);
-        int y = rnd.nextInt(sizeY);
-        pos = new Position(x, y);
+            posCount = 0;
+        } else if (posCount == 0) {
+            pos = new Position(nextX, nextY);
+            ++nextX;
+            if (nextX >= sizeX) {
+                nextX = 0;
+                ++nextY;
+                if (nextY >= sizeY) {
+                    nextY = 0;
+                }
+            }
+            posArray.add(pos);
+        }
+
+//        int x = rnd.nextInt(sizeX);
+//        int y = rnd.nextInt(sizeY);
+//        pos = new Position(x, y);
 
         return pos;
     }
